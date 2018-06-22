74da35b72dadf6a38e6ae70827ecfaa02f1568b9
Tyler Gubala <gubalatyler@yahoo.com>
2018-06-22 18:48:30 -0400

    push for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bpybuild",
-    version="1.0.0",
+    version="1.0.0a0",
     packages=find_packages(),
     author="Tyler Gubala",
     author_email="gubalatyler@gmail.com",

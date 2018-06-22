dd6d98aa8b444930aa494168015292cd12bfd3d0
Tyler Gubala <gubalatyler@yahoo.com>
2018-06-22 19:03:32 -0400

    upped version for the fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bpybuild",
-    version="1.0.0a0",
+    version="1.0.1",
     packages=find_packages(),
     author="Tyler Gubala",
     author_email="gubalatyler@gmail.com",

15048b92392bd7075d41e71d6a6857ec9f6b66ba
Tyler Gubala <gubalatyler@yahoo.com>
2018-06-26 15:31:47 -0400

    upped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bpybuild",
-    version="1.0.1",
+    version="1.0.1a0",
     packages=find_packages(),
     author="Tyler Gubala",
     author_email="gubalatyler@gmail.com",
